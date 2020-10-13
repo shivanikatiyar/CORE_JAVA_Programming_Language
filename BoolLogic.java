3665085fa08c549d20f6a6b58c9517b5ccd5bd3f
Shivani Katiyar <68283885+shivanikatiyar@users.noreply.github.com>
2020-10-13 17:30:44 +0530

    Boolean Logical operator program
    Boolean Logical operator program code

---
+++ BoolLogic.java
@@ -0,0 +1,9 @@
+//Example of Boolean logical operators
+public class BoolLogic{
+public static void main(String[] args){
+boolean bool1 = true,bool2 = false;
+System.out.println("bool1&&bool2="+(bool1&&bool2));
+System.out.println("bool1||bool2="+(bool1||bool2));
+System.out.println("!(bool1&&bool2)="+!(bool1&&bool2));
+}
+    }
