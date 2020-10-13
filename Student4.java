a1478ca980ef9d3aab5f9a2324af9c389512f0be
Shivani Katiyar <68283885+shivanikatiyar@users.noreply.github.com>
2020-10-13 17:43:47 +0530

    Parameterized Constructor program
    Parameterized Constructor program code

---
+++ Student4.java
@@ -0,0 +1,22 @@
+//Java parameteized constructor
+class Student4 {
+int id;
+String name;
+// Creating a parameteized constructor
+Student4(int i,String n)
+{
+id = i;
+name = n;
+}
+//Method to display the values 
+void display(){System.out.println(id+" "+name);}
+public static void main(String args[]){
+Student4 s1 = new Student4(111, "Rahul");
+Student4 s2 = new Student4(222, "Yash");
+s1.display();
+s2.display();
+}
+}
+
+
+
