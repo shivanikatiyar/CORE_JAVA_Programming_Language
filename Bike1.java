148063e1df60a905c0a2e1206709ac93df220643
Shivani Katiyar <68283885+shivanikatiyar@users.noreply.github.com>
2020-10-13 17:41:34 +0530

    Default Constructor Program code
    Default Constructor Program code

---
+++ Bike1.java
@@ -0,0 +1,9 @@
+class Bike1 {
+// Creating a default constructor
+Bike1(){System.out.println("Bike is created");}
+public static void main(String args[])
+{
+//calling a default constructor
+Bike1 b = new Bike1();
+ }
+}
