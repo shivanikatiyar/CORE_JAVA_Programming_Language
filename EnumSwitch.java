3c4eaf7b1868058292e96b55973347119618d82a
Shivani Katiyar <68283885+shivanikatiyar@users.noreply.github.com>
2020-10-16 23:18:44 +0530

    Enum in a switch case (even and odd) program
    Enum in a switch case (even and odd) program code

---
+++ EnumSwitch.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+enum Type{
+EVEN,
+ODD
+}
+public class EnumSwitch 
+{
+public static void main(String[] args){
+	Type t;
+Scanner myobj = new Scanner(System.in);
+System.out.println("Enter the Number Between 1 to 100:");
+int num = myobj.nextInt();
+if(num%2==0)
+	t=Type.EVEN;
+else
+	t=Type.ODD;
+switch(num%2){
+case 0:
+System.out.println("The number is :"+t.EVEN);
+break;
+case 1:
+System.out.println("The number is:"+t.ODD);
+break;
+default:
+}
+}
+}
