40036229222e72c5cabacb27b3ca47837499efa0
Shivani Katiyar <68283885+shivanikatiyar@users.noreply.github.com>
2020-10-21 21:17:24 +0530

    Factorial code using Recursion

---
+++ RecusFac.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class RecursionFact
+{
+static int factorial(int n)
+{
+if(n==1)
+return 1;
+else
+return(n*factorial(n-1));
+}
+public static void main(String[] args)
+{
+Scanner fact = new Scanner(System.in)
+System.out.println("Enter the Number:);
+int num = fact.nextInt();
+System.out.println(factorial());
+}
+}
