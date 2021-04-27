c5ccf0f1adef63c14b50ab8bb569a93c33dfc00f
Shivani Katiyar <68283885+shivanikatiyar@users.noreply.github.com>
2021-04-27 23:35:54 +0530

    Encapsulation

---
+++ Encapsulation.java
@@ -0,0 +1,13 @@
+public class Encapsulation {
+    public static void main(String args[])
+    {
+        student s=new student();
+        s.setroll(101);
+        s.setname("pooja");
+        s.setfee(2253);
+        s.setgen('f');
+        System.out.println("");
+        
+    }
+    
+}
